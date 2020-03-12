1b00a173d4645c7ee88a80197c38cb2eedf8cc57
Leonardo Leite Meira dos Santos <leonardo.meira@dtidigital.com.br>
2020-03-12 11:49:30 -0300

    First commit

---
+++ src/AboutPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <>
+            <h1>About me</h1>
+            <p>In this page there are somethings about me</p>
+        </>
+    );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import ContactPage from './ContactPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav>
+        <div className="linkNavBar">
+          <Link to="/Home">Home Page </Link>
+        </div>
+
+        <div className="linkNavBar">
+          <Link to="/About"> About </Link>
+        </div>
+
+        <div className="linkNavBar">
+          <Link to="/Contact"> Contact </Link>
+        </div>
+
+      </nav>
+
+      <main id="main">
+        <Switch>
+          <Route path="/Home" component={HomePage} />
+          <Route path="/About" component={AboutPage} />
+          <Route path="/Contact" component={ContactPage} />
+        </Switch>
+      </main>
+
+      <footer>
+        Footer of the page
+      </footer>
     </div>
   );
 }
